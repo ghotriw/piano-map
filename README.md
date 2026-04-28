e4f8954d4bd9703ca3a502657defc214c5a46828
Andrii Honcharov <dev@honcharov.pro>
2026-04-28 14:42:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Piano Map
+Interactive reference for piano chords & scales with audio playback and visual keyboard
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__title">🎹 Piano Map</h1>
+    </div>
+  )
+}
+
+export default App
